a851e45a95821094c321716d84d0211576df6758
DeutscherDude <kolokius@gmail.com>
2022-04-27 23:14:19 +0200

    Adding basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,11 @@ import { useLoadScript } from "@react-google-maps/api";
 import Map from "./Components/Map";
 import Search from "./Components/Search";
 import Sidebar from "./Components/Sidebar";
-import { BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Main from "./Components/Main";
 
 export default function App() {
-
-  const [ libraries ] = useState(['places']);
+  const [libraries] = useState(["places"]);
 
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_API_KEY,
@@ -34,13 +34,23 @@ export default function App() {
   return isLoaded ? (
     <BrowserRouter>
       <Routes>
-        <Route exact path="/" element={
-        <div>
-          <Sidebar distance={distance} duration={duration} />
-          <Search childToParent={updateParent} />
-          <Map directions={lookupValues} childToParent={getMetrics} />
-        </div>
-        }/>
+        <Route
+          exact path="/map"
+          element={
+            <div>
+              <Sidebar distance={distance} duration={duration} />
+              <Search childToParent={updateParent} />
+              <Map directions={lookupValues} childToParent={getMetrics} />
+            </div>
+          }
+        />
+        <Route
+          exact path="/"
+          element={
+          <div>
+          <Main childToParent={updateParent} />
+          </div>}
+        />
       </Routes>
     </BrowserRouter>
   ) : null;
